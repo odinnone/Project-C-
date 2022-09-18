4179ffb5936f68e98fc6332ab13cba8995c2c509
odinnone <vladborovkov01@mail.ru>
2022-09-18 12:09:03 +0300

    add Project 9 - Является ли одно число квадратом другого

---
+++ Project  9/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите два числа");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+if (y * y == x) {
+    Console.WriteLine($"Число {x} является квадратом числа {y}");
+}
+else {
+    Console.WriteLine($"Число {x} не является квадратом числа {y}");
+}
