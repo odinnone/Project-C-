ed3409e7a424929aa1b3d296b50f22ba22e375ec
odinnone <vladborovkov01@mail.ru>
2022-09-18 13:59:15 +0300

    add Project 13 - Сравнение двух чисел

---
+++ Project 13/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите два числа");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > y) {
+    Console.Write($"max = {x}, min = {y}");
+}
+else {
+    Console.Write($"max = {y}, min = {x}");
+}
