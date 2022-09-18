7c310e3ddd58ddebb9b1d13361286bea97c998c9
odinnone <vladborovkov01@mail.ru>
2022-09-18 14:03:44 +0300

    add Project 14 - Сравнение трех чисел

---
+++ Project 14/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите три числа");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int z = Convert.ToInt32(Console.ReadLine());
+int max = x;
+if (x > max) max = x;
+if (y > max) max = y;
+if (z > max) max = z;
+Console.Write($"max = {max}");
