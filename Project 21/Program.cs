2d553f623578224b80c80cd74dff9cfc6b8783bf
odinnone <vladborovkov01@mail.ru>
2022-09-24 14:25:53 +0300

    add Project 21 - вторая цифра трехзначного числа

---
+++ Project 21/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000) {
+    int digit = number / 10;
+    int result = digit % 10;
+    Console.WriteLine(result);
+}
+else Console.WriteLine("Введено не трехзначное число");
