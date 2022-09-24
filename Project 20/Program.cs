ec52712d12e412c3ac20e2403f246649a01b4cc2
odinnone <vladborovkov01@mail.ru>
2022-09-24 14:10:47 +0300

    add Project 20 - сравнение цифр двухзначного числа

---
+++ Project 20/Program.cs
@@ -0,0 +1,7 @@
+﻿int number = new Random().Next(10, 100);
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+Console.WriteLine(number);
+if (secondDigit == firstDigit) Console.WriteLine("Цифры одинаковые")
+else if (firstDigit > secondDigit) Console.WriteLine(firstDigit);
+else Console.WriteLine(secondDigit);
