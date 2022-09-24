83a6bf04ae5254bba14d7fb7ac505200eaafd915
odinnone <vladborovkov01@mail.ru>
2022-09-24 12:27:49 +0300

    add Project 19 - работа с массивами и функциями тест

---
+++ Project 19/Program.cs
@@ -0,0 +1,40 @@
+﻿void Array(int[] collection) {
+    int length = collection.Length;
+    int index = 0;
+    while (index < length) {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+} 
+
+void Print(int[] col) {
+    int count = col.Length;
+    int position = 0;
+    while(position < count) {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int Index(int[] collection, int find) {
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index < count) {
+        if (collection[index] == find) {
+            position = index;
+            break;
+        }
+        index++;
+        
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+Array(array);
+Print(array);
+Console.WriteLine();
+int pos = Index(array, 4);
+Console.WriteLine(pos);
