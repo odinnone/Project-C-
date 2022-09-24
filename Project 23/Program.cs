ad997de57e2d6873c13b3f8fd06b7e623ec34607
odinnone <vladborovkov01@mail.ru>
2022-09-24 14:41:20 +0300

    add Project 23 - выходной ли день по номеру дня недели

---
+++ Project 23/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите номер дня недели(1-7)");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 8) if (number == 6 || number == 7) Console.WriteLine("да");
+                else Console.WriteLine("нет");
+else Console.WriteLine("Ввели некорректное число");
+   
