d5e29e430abdab12b2f8795d6df3b649e8575898
odinnone <vladborovkov01@mail.ru>
2022-09-24 14:33:17 +0300

    add Project 22 - третья цифра числа

---
+++ Project 22/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99) {
+    int digit = number / 100;
+    int result = digit % 10;
+    Console.WriteLine(result);
+}
+else Console.WriteLine("Третьей цифры нет");
