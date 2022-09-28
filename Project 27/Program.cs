bb429c391164c537c6e2ac58119b8ce14edd5d19
odinnone <vladborovkov01@mail.ru>
2022-09-28 15:13:57 +0300

    add Project 27 - таблица кубов

---
+++ Project 27/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n >= 1) for (int i = 1; i <= n; i++) Console.Write($"{i*i*i} ");
+else Console.WriteLine("Ввели не пятизначное число");
