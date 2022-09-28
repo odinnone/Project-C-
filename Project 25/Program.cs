096b8f1c2e4a3c3f1b5ef68d7f1f18531c8c0bea
odinnone <vladborovkov01@mail.ru>
2022-09-28 11:20:09 +0300

    add Project 25 - пятизначное число палиндром

---
+++ Project 25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+string str = Console.ReadLine();
+int length = str.Length;
+int count = 0;
+if (length == 5) {
+    for (int i = 0; i < length / 2; i++) {
+        if (str[i] == str[length - i - 1]) count++;
+    }
+    if (count == length / 2) Console.WriteLine("палиндром");
+    else Console.WriteLine("не палиндром");
+}
+else Console.WriteLine("Ввели не пятизначное число");
