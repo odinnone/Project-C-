11006965392aa5fa7ff9ce3ff7fc38c8397bb9de
odinnone <vladborovkov01@mail.ru>
2022-10-03 18:38:28 +0300

    add Project 29 - Сумма цифр введенного числа

---
+++ Project 29/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+n = Math.Abs(n);
+int r = n % 10;
+while (n > 9) {
+    n = n / 10;
+    r = r + n % 10;
+}
+Console.WriteLine($"Сумма цифр введенного числа - {r}");
