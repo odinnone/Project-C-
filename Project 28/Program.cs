01a51d0b1ede19dad3c6d87c4e7dd60aa92d019a
odinnone <vladborovkov01@mail.ru>
2022-10-03 18:23:24 +0300

    add Project 28 - А в степени В

---
+++ Project 28/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите два числа(A и B)");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int r = 1;
+if (b > 0) {
+    for (int i = 0; i < b; i++) r = r*a;
+    Console.WriteLine($"Число А в степени В - {r}");
+}
+else Console.WriteLine("Ввели не натуральное число В"); 
