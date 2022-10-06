aa0a0d09bc51a96a6a267f96bf2c531d0eb837ea
odinnone <vladborovkov01@mail.ru>
2022-10-06 14:03:44 +0300

    add Project 31 - Кол-во четных элементов массива

---
+++ Project 31/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Mассив: ");
+PrintArray(numbers);
+int count = 0;
+for (int j = 0; j < size; j++) if (numbers[j] % 2 == 0) count++; 
+Console.Write("Колличество четных чисел - " + count);
+
+void FillArrayRandomNumbers(int[] numbers) {
+    for(int i = 0; i < numbers.Length; i++) numbers[i] = new Random().Next(100,1000);
+}
+
+void PrintArray(int[] numbers) {
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++) Console.Write(numbers[i] + " ");
+    Console.Write("]");
+    Console.WriteLine();
+}
