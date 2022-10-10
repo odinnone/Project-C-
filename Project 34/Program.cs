6e3ca8b2e6f1807560e102f44319602acb8c420a
odinnone <vladborovkov01@mail.ru>
2022-10-10 17:02:28 +0300

    add Project 34 - Количество чисел > 0

---
+++ Project 34/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите числа(через пробел) ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+for (int i = 0; i < arr.Length; i++) if (arr[i] > 0) count++;
+Console.WriteLine($"Количество чисел > 0 - {count}");
