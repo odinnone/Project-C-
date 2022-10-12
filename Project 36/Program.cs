1e8199903f11e1910f886c7ee0ed85fc6e85e2d2
odinnone <vladborovkov01@mail.ru>
2022-10-12 18:21:58 +0300

    add Project 36 - Задайте двумерный массив размером m×n, заполненный случайными вещественными числами

---
+++ Project 36/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите кол-во строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[m, n];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+void FillArrayRandomNumbers(double[,] array) {
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+    }
+}
+void PrintArray(double[,] array) {
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for (int j = 0; j < array.GetLength(1); j++) Console.Write(array[i, j] + " ");
+        Console.WriteLine("");
+    }
+}
